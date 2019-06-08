82f0a913ae0e1787363ace65ff7385b238cce89a
Chris Salvi <csalvi42@gmail.com>
2019-06-08 05:52:19 -0700

    user registration

--- server.js
+++ server.js
@@ -1,11 +1,15 @@
 const express = require('express');
 const connectDB = require('./config/db');
 
+
 const app = express();
 
 // Connect DB
 
 connectDB();
+
+// Init Middleware
+app.use(express.json({ extended: false }));
 
 app.get('/', (req, res) => res.send('API Running'));
 
